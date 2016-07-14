588c979020dd14c0c5cf1a230a419fcb1f08092e
张浩 <zhanghao@zhanghaodeMacBook-Pro.local>
2016-07-14 09:27:43 +0800

    commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var PROCESSES = {
     'extend-uri' : require('./process/extend-uri.js')
 };
 
-var CHINAS = ['extend-uri'];
+var CHINAS = ['extend-uri', 'widget'];
 
 module.exports = function(content, file, conf){
 	CHINAS.forEach(function(item){
